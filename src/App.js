12c2256b1d03f8afa4e015f54fb552bc8a0bd9c4
abdul <abdulsaeed1356@gmail.com>
2023-05-30 18:00:53 +0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import Header from './component/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <>
+  <Header/>
+  <Routes>
+
+    <Route path='/' element={<Home/>}/>
+    <Route path='/Contact' element={<Contact/>}/>
+    <Route path='/About' element={<About/>} />
+    <Route/>
+  </Routes> 
+ </>
+ )
 }
 
 export default App;
